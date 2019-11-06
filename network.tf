4a27dd87ecbbafeff4f27a604a1fab2670380db2
Nic Jackson <jackson.nic@gmail.com>
2019-11-06 17:06:35 +0000

    Update to use default vpc

--- network.tf
+++ network.tf
@@ -6,7 +6,6 @@ resource "aws_default_vpc" "default" {
 
 resource "aws_default_subnet" "default_az1" {
   availability_zone = "us-east-1a"
-  vpc_id            = aws_default_vpc.default.id
 
   tags = {
     Name = "Default subnet for us-east-1a"
