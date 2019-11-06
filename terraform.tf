cd4a6999d937f13b0774ebdeaa2e77547c670380
Nic Jackson <jackson.nic@gmail.com>
2019-11-06 16:14:33 +0000

    Added remote state config

--- terraform.tf
+++ terraform.tf
@@ -1,3 +1,14 @@
+terraform {
+  backend "remote" {
+    hostname = "app.terraform.io"
+    organization = "hashicorp-team-da-beta"
+
+    workspaces {
+      name = "demo-consul-cloud-map"
+    }
+  }
+}
+
 resource "aws_vpc" "example" {
   cidr_block           = "10.0.0.0/16"
   enable_dns_support   = true
