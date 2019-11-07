1f0f363f5a575e48c150e75b4a77afbce65c65a5
jacksonnic <jacksonnic@penguin>
2019-11-07 16:48:50 -0700

    rename key

--- terraform.tf
+++ terraform.tf
@@ -15,6 +15,6 @@ resource "tls_private_key" "deployer" {
 }
 
 resource "aws_key_pair" "deployer" {
-  key_name   = "deployer-key"
+  key_name   = "deployer"
   public_key = tls_private_key.deployer.public_key_openssh 
 }
