c3a6b74be571c2c6c2398f5cfd68c690712affc4
jacksonnic <jacksonnic@penguin>
2019-11-08 15:58:47 -0700

    add payments to output

--- outputs.tf
+++ outputs.tf
@@ -2,8 +2,16 @@ output "consul_server" {
   value = aws_instance.consul_server.public_ip
 }
 
+output "web" {
+  value = aws_instance.web.public_ip
+}
+
 output "api" {
   value = aws_instance.api.public_ip
+}
+
+output "payments" {
+  value = aws_instance.payments.public_ip
 }
 
 output "private_key" {
