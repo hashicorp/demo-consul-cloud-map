9fdda8309a2688de0c50af2d6a1589702c84828a
Nic Jackson <jackson.nic@gmail.com>
2019-11-11 17:12:10 +0000

    Updated to enable nat

--- network.tf
+++ network.tf
@@ -9,6 +9,7 @@ module "vpc" {
   public_subnets     = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
 
   enable_dns_hostnames = true
+  enable_nat_gateway   = true
 
   tags               = {
     Terraform        = "true"
