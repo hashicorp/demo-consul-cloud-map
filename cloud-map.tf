462e8f726050ecdd38bf95b37e661ce0f0729a7a
Nic Jackson <jackson.nic@gmail.com>
2019-11-11 17:35:41 +0000

    Updated domain name

--- cloud-map.tf
+++ cloud-map.tf
@@ -1,5 +1,5 @@
 resource "aws_service_discovery_public_dns_namespace" "example" {
-  name        = "example.terraform.local"
+  name        = "example.terraform"
   description = "example"
   #vpc         = aws_default_vpc.default.id
 }
