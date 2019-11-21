b8f6af9c3114f4028f54896be17dd9150c5a5ead
Rosemary Wang <915624+joatmon08@users.noreply.github.com>
2019-11-21 16:16:51 -0500

    Update with api-on-aws service

--- cloud-map.tf
+++ cloud-map.tf
@@ -3,3 +3,22 @@ resource "aws_service_discovery_private_dns_namespace" "example" {
   description = "example"
   vpc         = aws_vpc.default.id
 }
+
+resource "aws_service_discovery_service" "api_on_aws" {
+  name = "api-on-aws"
+
+  dns_config {
+    namespace_id = aws_service_discovery_private_dns_namespace.example.id
+
+    dns_records {
+      ttl  = 10
+      type = "A"
+    }
+
+    routing_policy = "MULTIVALUE"
+  }
+
+  health_check_custom_config {
+    failure_threshold = 1
+  }
+}
