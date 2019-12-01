301e5dc1c76a46cc7b92164b47fc073a8e6fef45
Rosemary Wang <915624+joatmon08@users.noreply.github.com>
2019-12-01 11:28:29 -0800

    Fix toggle for web

--- toggles.tf
+++ toggles.tf
@@ -2,8 +2,8 @@ variable "enable_web_on_aws" {
   default = false
 }
 
-variable "disable_service_mesh_on_aws" {
-  default = false
+variable "enable_service_mesh_on_aws" {
+  default = true
 }
 
 variable "fix_api_on_aws" {
