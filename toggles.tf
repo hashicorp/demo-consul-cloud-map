08451e16f192ceeb8889b1e02f0936bb977f35dd
Rosemary Wang <915624+joatmon08@users.noreply.github.com>
2019-12-03 12:53:36 -0800

    Revert toggles

--- toggles.tf
+++ toggles.tf
@@ -1,11 +1,11 @@
 variable "fix_api_on_aws" {
-  default = true
-}
-
-variable "use_consul_proxy_for_api" {
   default = false
 }
 
-variable "enable_web_on_aws" {
+variable "use_consul_proxy_for_api" {
   default = true
+}
+
+variable "enable_web_on_aws" {
+  default = false
 }
